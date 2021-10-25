0df68c9ae19e5d349c9b7b6188fdb32f4c6338a0
Karl Stolley <karl.stolley@gmail.com>
2021-10-25 17:35:29 -0400

    Add REPL input for trueness/falseness

---
+++ truthiness.repl.js
@@ -0,0 +1,46 @@
+5 + 5;
+"5" + "5";
+"some" + "where";
+"5" + "cow";
+5 + "cow";
+"cow" + 5;
+5 - 5;
+5 - "cow"let
+5 - "cow";
+"cow" - 5;
+5 == "5";
+5 === "5";
+7 != "7";
+7 !== "7";
+5 * "cow";
+"cow" * 5;
+5 / 7;
+5 / "cow";
+"cow" / 5;
+typeof(NaN);
+isNaN(5);
+isNaN("cow");
+isNaN("cow"/5);
+isNaN(NaN);
+
+typeof(NaN);
+NaN == NaN;
+NaN === NaN;
+(5/"cow") == ("cow"/5);
+!false;
+!true;
+!!false;
+!!true;
+!7;
+!!7;
+!!false;
+!!"cow";
+!!"0"let
+!!"0";
+!!0;
+!!0.00000001;
+!!"0";
+!!"";
+!!NaN;
+!!0;
+!!false;

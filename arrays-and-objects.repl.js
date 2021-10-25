747025e403fd142632dfa703ce7a3e6cf96349a0
Karl Stolley <karl.stolley@gmail.com>
2021-10-25 17:51:02 -0400

    Add REPL input arrays and objects

---
+++ arrays-and-objects.repl.js
@@ -0,0 +1,24 @@
+var name = "Karl";
+var dog = "Hank";
+var stooge1 = "Moe";
+var stooge2 = "Larry";
+var stooge3 = "Curly";
+var stooge4 = "Shemp";
+var stooges = ['Moe','Curly','Larry','Shemp'];
+typeof stooges;
+stooges;
+stooges.length;
+var beatles = ['Paul','Ringo','John','George'];
+beatles.length;
+var temperatures = [87,92,45,36];
+var person = {
+name: "Karl",
+dog: "Hank"
+};
+person;
+beatles[0];
+beatles[2]'
+beatles[2];
+beatles;
+person.name;
+person.dog;

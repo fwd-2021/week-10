2c982325b796bf41bce1bdbee018ad056be830c1
Karl Stolley <karl.stolley@gmail.com>
2021-10-25 17:12:46 -0400

    Add REPL input for basic JS types

---
+++ basic-types.repl.js
@@ -0,0 +1,42 @@
+console.clear();
+5;
+5
+5
+5;
+5.0;
+5.0;
+5.01;
+5.02;
+5.2;
+5.20;
+520;
+typeof(5);
+typeof(5.01);
+"cat";
+"dog";
+"cow";
+"cat";
+'cat';
+`cat`;
+"That's really interesting";
+'That's really interesting';
+
+"That's really great";
+'That\'s really cumbersome';
+'I love the poem "Roses Are Red"';
+"I love the poem \"Roses Are Red\"";
+"500";
+typeof(500);
+typeof('500');
+60616;
+"60616";
+"8005882300"
+8005882300;
+typeof('cat');
+typeof('dog');
+typeof(5);
+false;
+true;
+typeof(true);
+typeof(false);
+'cat';

c65ecf99b59a2fff923876cd9bb02d7bbd6bc76e
Karl Stolley <karl.stolley@gmail.com>
2021-10-27 16:06:08 -0500

    Add example JavaScript file organization

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Global variables
+var importantData = {
+  so: 'important'
+};
+
+// Function statements, class statements
+var someFancyFunction = function() {
+  // And here we get fancy
+};
+var SomeFancyClass = class() {
+  // create a fancy class
+};
+var SomeOldSchoolClass = function() {
+  // create an old-school "class"
+};
+
+// Down to business!
+var myFancyClass = new SomeFancyClass();
+var myOldSchoolClass = new SomeOldSchoolClass();
+
+someFancyFunction(importantData);
+someWorkHorseFunction();
+
+// Function expressions
+function someWorkHorseFunction() {
+  // And here we get down to work...
+}
